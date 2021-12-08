393b1a8c2c7dc81c6134f76d525a1d236ba7157f
matheuscosta22 <matheuscosta2539@gmail.com>
2021-12-08 20:02:48 -0300

    first commit

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ var bodyParser = require('body-parser')
 var express = require("express")
 var app = express()
 var router = require("./routes/routes")
+const cors = require('cors')
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
-
 app.use(bodyParser.json())
-
 app.use("/api", router);
-
 app.listen(8686, () => {
     console.log("Server on")
 });
